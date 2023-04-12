37edb8a8eb3fdbfe6617e94cb5d65ed390d6df23
Benah0987 <wanyoikebenayah77@gmail.com>
2023-04-12 07:29:25 +0300

    added a reduce() method

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+
+
+//introduced a new var for the reducer to store its value
+//added two parameter a callback functon and an accumulator
+//the callback updates thr value of the accumulator
+const totalBatteries = batteryBatches.reduce((accumulator, currentValue) => 
+                 {
+  return accumulator + currentValue;
+}, 0);
+
+console.log(`The total number of batteries is ${totalBatteries}.`);
+
